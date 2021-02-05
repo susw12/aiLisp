58fcd73ea6f295177a577ac5a815fc6e1baa3362
Sujay Swain <susw12@noreply.github.io>
2021-02-05 00:27:15 -0500

    Hw 1 Finish

--- worksheet1.lisp
+++ worksheet1.lisp
@@ -8,4 +8,12 @@
 
 ; Question 2
 (car (cdr (cdr (cdr '(he is dead jim)))))
-(cdr (car '(car (captain (((jim) kirk))))))
+(car (car (car (car (cdr '(captain (((jim) kirk))))))))
+(car (cdr (car (car '(((((spock) asked) jim) if) he was all right)))))
+(car (car (car (car (car (cdr (cdr (cdr (cdr (car (cdr '(after (looking at the (lizard man) ((((jim))) asked for warp 9))))))))))))))
+
+; Question 3
+(reverse '(he is dead jim))
+(reverse '(captain (((jim) kirk))))
+(reverse '(((((spock) asked) jim) if) he was all right))
+(reverse '(after (looking at the (lizard man) ((((jim))) asked for warp 9))))
